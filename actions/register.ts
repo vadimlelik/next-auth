2145120511d478cc23eb1aed213031fd2d8e70a4
vadim <cvi-vadim@yandex.ru>
2024-02-02 11:47:38 +0300

    add:server action register

---
+++ actions/register.ts
@@ -0,0 +1,11 @@
+import { RegisterSchema } from "@/schemas";
+import * as z from "zod";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validateFields = RegisterSchema.safeParse(values);
+  if (!validateFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  return { success: "Email sent" };
+};
