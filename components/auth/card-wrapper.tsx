4e4c5eab7259a946c4d6b135cadc36c1f4b8a674
vadim <cvi-vadim@yandex.ru>
2024-02-02 11:54:44 +0300

    Revert "add:UI wrapper button"
    This reverts commit 4276b1e1d6d82a0437007b67867e96eaa5e74a13.

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,48 +1,37 @@
-"use client";
-import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "../ui/card";
-import Header from "./header";
-import Social from "./social";
-import BackButton from "./back-button";
+'use client';
+import React from 'react';
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '../ui/card';
+import Header from './header';
+import Social from './social';
 
 interface CardWrapperProps {
-  children: React.ReactNode;
-  headerLabel: string;
-  backBUttonLabel: string;
-  backButtonHref: string;
-  showSocial?: boolean;
+    children: React.ReactNode;
+    headerLabel: string;
+    backBUttonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
 }
 
 const CardWrapper = ({
-  children,
-  backBUttonLabel,
-  backButtonHref,
-  headerLabel,
-  showSocial,
+    children,
+    backBUttonLabel,
+    backButtonHref,
+    headerLabel,
+    showSocial,
 }: CardWrapperProps) => {
-  return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label={headerLabel} />
-      </CardHeader>
-      <CardContent>{children}</CardContent>
-      {showSocial && (
-        <CardFooter>
-          <Social />
-        </CardFooter>
-      )}
-      <CardFooter>
-        <BackButton label={backBUttonLabel} href={backButtonHref} />
-      </CardFooter>
-    </Card>
-  );
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerLabel} />
+            </CardHeader>
+            <CardContent>{children}</CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+        </Card>
+    );
 };
 
 export default CardWrapper;
